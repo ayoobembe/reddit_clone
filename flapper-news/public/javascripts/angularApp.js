21edbc064eff8102da25f77ecfbcb5a41a403c6f
Ayo <ayo.obembe@gmail.com>
2015-04-17 10:23:18 +0100

    wired up angular front end and express back end, posts and comments now being updated on front and queried from database

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -1,5 +1,9 @@
 var app = angular.module('flapperNews', ['ui.router']);
 
+
+
+
+
 app.config([
   '$stateProvider',
   '$urlRouterProvider',
@@ -9,17 +13,33 @@ app.config([
       .state('home', {
         url: '/home',
         templateUrl: '/home.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        resolve: {
+          postPromise: ['posts', function(posts) {
+            return posts.getAll();
+          }]
+        }
       })
 
       .state('posts', {
         url: '/posts/{id}',
         templateUrl: '/posts.html',
-        controller: 'PostsCtrl'
+        controller: 'PostsCtrl',
+        resolve: {
+          post: ['$stateParams', 'posts',
+            function($stateParams, posts) {
+              return posts.get($stateParams.id);
+            }]
+        }
       });
 
     $urlRouterProvider.otherwise('home');
   }]);
+
+
+
+
+
 
 app.controller('MainCtrl', [
   '$scope',
@@ -31,39 +51,69 @@ app.controller('MainCtrl', [
 
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
+      posts.create({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0,
-        comments: [
-          {author: 'Joe', body: 'Cool post!', upvotes: 0},
-          {author: 'Bob', body: 'Great idea, but everything is wrong!', upvotes: 0}
-        ]
-        });
+      });
       $scope.title = '';
       $scope.link = '';
     };
 
     $scope.incrementUpvotes = function(post){
-      post.upvotes += 1;
+      posts.upvote(post);
     };
 
   }]);
 
-  app.factory('posts', [function() {
+
+
+
+
+  app.factory('posts', ['$http', function($http) {
     var o = {
       posts: []
     };
+
+    o.getAll = function() {
+      return $http.get('/posts').success(function(data) {
+        angular.copy(data, o.posts);
+      });
+    };
+
+    o.create = function(post) {
+      return $http.post('/posts', post).success(function(data){
+        o.posts.push(data);
+      });
+    };
+
+    o.upvote = function(post) {
+      return $http.put('/posts/' + post._id + '/upvote' )
+        .success(function(data){
+          post.upvotes += 1;
+        });
+    };
+
+    o.get = function(id) {
+      return $http.get('/posts/' + id).then(function(res){
+        return res.data;
+      });
+    };
+
     return o;
   }]);
 
 
+
+
+
+
+
   app.controller('PostsCtrl', [
     '$scope',
-    '$stateParams',
     'posts',
-    function($scope, $stateParams, posts) {
-      $scope.post = posts.posts[$stateParams.id];
+    'post',
+    function($scope, posts, post) {
+      $scope.post = post;
 
       $scope.addComment = function() {
         if($scope.body==='') { return; }
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -33,4 +33,82 @@ router.post('/posts', function(req, res, next) {
 });
 
 
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function(err, post) {
+    if(err) { return next(err); }
+    if(!post) {return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+
+// testing
+// test22: 552e91e8b07e69d705a88b6f
+// comment = test: 552fbb313ce3153609d4c7e8
+
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function(err, comment) {
+    if(err) { return next(err); }
+    if(!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment
+    return next()
+  });
+});
+
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if(err) {return next(err); }
+
+    res.json(comment);
+  });
+});
+
+
+//don't think it's loading the comments properly ... check later.
+router.get('/posts/:post', function(req, res) {
+  req.post.populate('comments', function(err, post) {
+    if(err) { return next(err); }
+
+    res.json(post);
+  });
+
+});
+
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+
+router.post('/posts/:post/comment', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment) {
+    if(err) { return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err) { return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+
+
 module.exports = router;
